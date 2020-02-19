ffb7d28a67cad6f88aa7516c63bd5a62fbd22cc7
张豪铭 <444871629@qq.com>
2020-02-19 19:42:08 +0800

    项目部署打包

--- oak-spring-boot/oak-rbac/src/main/java/com/oak/rbac/controller/MenuController.java
+++ oak-spring-boot/oak-rbac/src/main/java/com/oak/rbac/controller/MenuController.java
@@ -65,14 +65,14 @@ public class MenuController {
      * @param dto
      * @return
      */
-    @GetMapping("/add_menu_info")
+    @GetMapping("/update_menu_info")
     @Operation(summary = "添加菜单", description = "添加菜单")
     public ApiResp<Void> updateMenuInfo(UpdateMenuInfoReq dto) {
         return menuManagementService.updateMenuInfo(dto);
     }
 
 
-    @GetMapping("/add_menu_info")
+    @GetMapping("/del_menu_info")
     @Operation(summary = "添加菜单", description = "添加菜单")
     public ApiResp<Void> delMenuInfo(DelMenuInfoReq dto) {
         return menuManagementService.delMenuInfo(dto);
