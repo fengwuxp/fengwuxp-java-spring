33864c18ba71aaa8c5268ac851772c2caf9a92e0
风 <1109695647@qq.com>
2020-02-27 18:04:43 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- api-support/src/main/java/com/wuxp/api/initiator/AbstractBaseInitiator.java
+++ api-support/src/main/java/com/wuxp/api/initiator/AbstractBaseInitiator.java
@@ -2,6 +2,10 @@ package com.wuxp.api.initiator;
 
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.BeanFactoryAware;
+import org.springframework.beans.factory.InitializingBean;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationStartedEvent;
 import org.springframework.context.ApplicationListener;
@@ -18,16 +22,16 @@ import java.util.concurrent.ConcurrentHashMap;
  */
 @Slf4j
 @Setter
-public abstract class AbstractBaseInitiator<T> implements ApplicationListener<ApplicationStartedEvent> {
+public abstract class AbstractBaseInitiator<T> implements ApplicationListener<ApplicationStartedEvent>, InitializingBean, BeanFactoryAware {
 
     protected List<T> initData;
+
+    protected BeanFactory beanFactory;
 
     // 默认使用异步执行
     protected boolean isAsync = true;
 
-    @Autowired
     protected ThreadPoolTaskScheduler threadPoolTaskScheduler;
-
 
     private static final Map<Class<? extends AbstractBaseInitiator>, Boolean> INITIATOR_MAP = new ConcurrentHashMap<>();
 
@@ -45,7 +49,7 @@ public abstract class AbstractBaseInitiator<T> implements ApplicationListener<Ap
         }
         INITIATOR_MAP.put(aClass, true);
 
-        if (this.isAsync) {
+        if (this.isAsync && this.threadPoolTaskScheduler != null) {
             this.threadPoolTaskScheduler.execute(this::init);
         } else {
             init();
@@ -56,6 +60,19 @@ public abstract class AbstractBaseInitiator<T> implements ApplicationListener<Ap
 
     public List<T> getInitData() {
         return initData;
+    }
+
+
+    @Override
+    public void afterPropertiesSet() throws Exception {
+        if (this.threadPoolTaskScheduler == null) {
+            try {
+                this.threadPoolTaskScheduler = this.beanFactory.getBean(ThreadPoolTaskScheduler.class);
+            } catch (BeansException e) {
+                e.printStackTrace();
+                this.isAsync = false;
+            }
+        }
     }
 
     public void setInitData(List<T> initData) {
