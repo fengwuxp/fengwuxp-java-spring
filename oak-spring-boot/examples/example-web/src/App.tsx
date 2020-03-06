74d8aa4a8965f64b872df6e509fe269936679810
风 <1109695647@qq.com>
2020-03-06 14:38:16 +0800

    Signed-off-by: 风 <1109695647@qq.com>

--- oak-spring-boot/examples/example-web/src/App.tsx
+++
@@ -1,9 +0,0 @@
-import * as ReactDOM from "react-dom";
-import React from "react";
-import {ScanLoginView} from "./views/ScanLoginView";
-
-
-const App = <ScanLoginView/>;
-
-ReactDOM.render(App,
-    document.getElementById("app"));
